feabc0acdcf855b1b1ac7340e6878486070393a3
klssvppvarad-oss <klssvppvarad@gmail.com>
2026-04-10 17:08:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+app = FastAPI()
+
+class URLRequest(BaseModel):
+    url: str
+
+def shorten_url(long_url: str) -> str:
+    try:
+        res = requests.get(
+            "https://tinyurl.com/api-create.php",
+            params={"url": long_url},
+            timeout=5
+        )
+
+        short_url = res.text.strip()
+
+        # Force correct domain (avoid preview subdomain issues)
+        if "tinyurl.com" in short_url:
+            short_url = short_url.replace("preview.", "")
+
+        return short_url
+
+    except:
+        return "error"
+
+@app.post("/shorten")
+def shorten(data: URLRequest):
+    return {
+        "original": data.url,
+        "short": shorten_url(data.url)
+    }
